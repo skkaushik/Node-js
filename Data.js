d17636f5ff2250eebe9734ef9b8bcfc7074a4145
sachin <sksachinkaushik5362@gmail.com>
2023-05-25 13:47:47 +0530

    creating sever

--- Data.js
+++ Data.js
@@ -1,8 +1,40 @@
 const data=[
     
-        {name:'Sachin Kauhsik',Email:'skkaushik5092@gmail.com',phone:9073848393},
-        {name:'Sahil Kumar',Email:'Sahilsir@gmail.com',phone:8398483930},
-        {name:'Rahul Dev',Email:'rahuldev123@gmail.com',phone:9875643202}
+        {
+            name:'Sachin Kauhsik',
+            Email:'skkaushik5092@gmail.com',
+            phone:9073848393,
+            gender:'Male'},
+
+        {
+        name:'Sahil Kumar',
+        Email:'Sahilsir@gmail.com',
+        phone:8398483930,
+        gender:'Male'
+    },
+
+        { 
+             name:'Monika Rawat'
+        ,Email:'Monikar@gmail.com',
+        phone:7196004839,
+        gender:'Female'
+    },
+    
+        {
+            name:'Rahul Dev',
+        Email:'rahuldev123@gmail.com',
+        phone:9875643202,
+        gender:'Male'
+    },
+        { 
+             name:'Usha Devi',
+        Email:'usha@gmail.com',
+        phone:6370048393,
+        gender:'Female'
+    }
+
+        
+            
     
 ];
 
