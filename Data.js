68c85a63a3cdedcd4e3d26f03d4da24c76862338
sachin <sksachinkaushik5362@gmail.com>
2023-05-25 13:33:58 +0530

    puttind data into data file and importing data from another file

---
+++ Data.js
@@ -0,0 +1,9 @@
+const data=[
+    
+        {name:'Sachin Kauhsik',Email:'skkaushik5092@gmail.com',phone:9073848393},
+        {name:'Sahil Kumar',Email:'Sahilsir@gmail.com',phone:8398483930},
+        {name:'Rahul Dev',Email:'rahuldev123@gmail.com',phone:9875643202}
+    
+];
+
+module.exports=data;
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require("http");
+const data=require('./Data')
 http.createServer((req,resp)=>{
     resp.writeHead(200,{'Content-Type':'application\json'});
-    resp.write(JSON.stringify({name:'Sachin Kauhsik',Email:'skkaushik5092@gmail.com'}));
+    resp.write(JSON.stringify(data));
     resp.end();
 
 }).listen(8080)
