6d48f84531afc0fb66ae3d6d14f2dad6c450dadd
sachin <sksachinkaushik5362@gmail.com>
2023-05-25 13:23:09 +0530

    First api using node

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const color = require("colors")
-console.log("sachin".red)
+const http = require("http");
+http.createServer((req,resp)=>{
+    resp.writeHead(200,{'Content-Type':'application\json'});
+    resp.write(JSON.stringify({name:'Sachin Kauhsik',Email:'skkaushik5092@gmail.com'}));
+    resp.end();
+
+}).listen(8080)
